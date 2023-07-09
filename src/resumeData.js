8e4ad76d6ad97a85df553643c77bb609898916cc
883411 <jacob.xing04@gmail.com>
2023-07-09 15:40:59 -0500

    Update resumeData.js

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,52 +1,46 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
+    "imagebaseurl":"https://jacob-xing.github.io",
+    "name": "Jacob Xing",
+    "role": "Computer Engineer",
+    "linkedinId":"jacobxing",
     "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/jacobxing/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"http://github.com/jacob-xing",
           "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
+    "aboutme":"I am currently a second year student at the University of Minnesota studying Compute Engineering.",
+    "address":"United States",
     "website":"https://rbhatia46.github.io",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
+        "UniversityName":"University of Minnesota",
+        "specialization":"Computer Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2026",
         "Achievements":"Some Achievements"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
+        "UniversityName":"Iowa State University",
+        "specialization":"Post Secondary Enrollment Option",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2022",
         "Achievements":"Some Achievements"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
+        "CompanyName":"27Stores",
+        "specialization":"Website Developer",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2022",
         "Achievements":"Some Achievements"
       },
       {
